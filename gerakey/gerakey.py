2a1a6b9d8e7f0364231da9f738d024ddd8097d73
TrexPD <leynad157@gmail.com>
2023-01-13 17:05:50 -0300

    alteração no vocabulário, add lib rich, alteração no arquivo .gpuf

--- gerakey/gerakey.py
+++ gerakey/gerakey.py
@@ -1,12 +1,16 @@
 from random import choices
 from string import ascii_letters, digits, punctuation
+from datetime import datetime
+from rich.console import Console
+from rich import print
+from time import sleep
 
 
 def gerador_keys(
     pontuacao: bool = True,
     numeros: bool = True,
     salvar: bool = False,
-    tamanho: int = 8,
+    tamanho: int = 12,
     remdigito: str = None,
     rempontuacao: str = None,
 ) -> str:
@@ -16,7 +20,7 @@ def gerador_keys(
         p = punctuation
         if rempontuacao != None:
             if rempontuacao.isalnum():
-                return "Typer error: 'rempontuacao' não aceita números ou letras do alfabeto!"
+                return "[red]TypeError:[/] 'rempontuacao' não aceita números ou letras do alfabeto!"
             else:
                 for remover in rempontuacao:
                     p = p.replace(f'{remover}', '')
@@ -28,19 +32,24 @@ def gerador_keys(
                 for remover in remdigito:
                     d = d.replace(f'{remover}', '')
             else:
-                return "Typer error: 'remdigito' aceita apenas números!"
+                return "[red]TypeError:[/] 'remdigito' aceita apenas números de 0-9!"
 
     senha: str = f"{''.join(choices(ascii_letters + p + d, k=tamanho))[::-1]}"
+    # Salva em um arquivo '.gpuf'(General Purpose File)
     if salvar:
-        with open('password.guf', 'at', encoding='utf-8') as arquivo:
-            print('Key:', senha, file=arquivo)
-            print(
-                "O seu token foi salvo no arquivo 'password.guf' com sucesso!\n"
-            )
+        data_atual = datetime.now().strftime(r"%d/%m/%Y %X.%f")
+        with open('password.gpuf', 'at', encoding='utf-8') as arquivo:
+            arquivo.write('[Token]:\n')
+            arquivo.write(f'\t"date/time": "{data_atual}";\n')
+            arquivo.write(f'\t"key": "{senha}";\n\n')
+            print("O seu [bold]token[/] foi salvo no arquivo [yellow]'password.gpuf'[/] com sucesso!")
 
-    return f'Key: {senha}\n' + '-' * 47
-
+    console = Console()
+    with console.status('Gerando sua [bold]key/token[/], aguarde...'):
+        sleep(1.5)
+        return f'\n[bold]Key/Token:[/] [yellow]{senha}[/]\n' + '-' * 61
+        
 
 if __name__ == '__main__':
-    senha: str = gerador_keys(remdigito='09', pontuacao=False, salvar=True, tamanho=15)
+    senha: str = gerador_keys(pontuacao=False, salvar=False, tamanho=15)
     print(senha)
